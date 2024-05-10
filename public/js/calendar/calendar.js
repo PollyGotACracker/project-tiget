819467d1d717606bb04b2860840fc806b2e131c5
PollyGotACracker <bitterns96@gmail.com>
2024-05-11 04:42:52 +0900

    refactor: Calendar 코드 모듈화

--- public/js/calendar/calendar.js
+++ public/js/calendar/calendar.js
@@ -4,30 +4,64 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnNext = document.querySelector("button.next");
   const btnToday = document.querySelector("button.today");
 
-  const time = new Date();
-  // 달력 넘기기 용도
-  const valDay = {
-    year: time.getFullYear(),
-    month: time.getMonth() + 1,
-    date: time.getDate(),
-    day: time.getDay(),
-  };
-  // 오늘 날짜 표시 용도
-  const today = {
-    year: time.getFullYear(),
-    month: time.getMonth() + 1,
-    date: time.getDate(),
-    day: time.getDay(),
+  class TodayDate {
+    constructor() {
+      const now = new Date();
+      this.year = now.getFullYear();
+      this.month = now.getMonth() + 1;
+      this.date = now.getDate();
+      this.day = now.getDay();
+    }
+  }
+
+  const selectedDate = new TodayDate();
+  const today = new TodayDate();
+
+  const setTodayText = () => {
+    const yearEle = document.querySelector("h2.year");
+    const monthEle = document.querySelector("h1.month");
+    yearEle.textContent = `${selectedDate.year}년`;
+    monthEle.textContent = `${selectedDate.month}월`;
   };
 
-  const showNum = () => {
-    const yearNum = document.querySelector("h2.year");
-    const monthNum = document.querySelector("h1.month");
-    yearNum.textContent = `${valDay.year}년`;
-    monthNum.textContent = `${valDay.month}월`;
+  const getFormattedDate = (year, month, date) => {
+    return `${year}-${String(month).padStart(2, 0)}-${String(date).padStart(
+      2,
+      0
+    )}`;
   };
 
-  const matchDay = (arr, val) => arr.includes(val);
+  const getDateRange = (selectedDate) => {
+    const isFirstMonth = selectedDate.month === 1;
+    const isLastMonth = selectedDate.month === 12;
+    const prev = {
+      year: isLastMonth ? selectedDate.year + 1 : selectedDate.year,
+      month: isLastMonth ? 1 : selectedDate.month - 1,
+      date: 1,
+    };
+    const next = {
+      year: isFirstMonth ? selectedDate.year - 1 : selectedDate.year,
+      month: isFirstMonth ? 12 : selectedDate.month + 1,
+      date: 31,
+    };
+    return {
+      prevDate: getFormattedDate(prev.year, prev.month, prev.date),
+      nextDate: getFormattedDate(next.year, next.month, next.date),
+    };
+  };
+
+  const fetchSchedule = async () => {
+    const { prevDate, nextDate } = getDateRange(selectedDate);
+    const fetchOption = {
+      method: "POST",
+      body: JSON.stringify({ prevDate, nextDate }),
+      headers: { "Content-Type": "application/json" },
+    };
+    const result = await fetch("/main/schedule", fetchOption).then((res) =>
+      res.json()
+    );
+    return result;
+  };
 
   const addSchedule = (concert) => {
     const schedule = document.createElement("div");
@@ -37,60 +71,14 @@ document.addEventListener("DOMContentLoaded", () => {
     return schedule;
   };
 
-  // const calcDate = (start, end) => {
-  //   let diffDate = new Date(end).getTime() - new Date(start).getTime();
-  //   diffDate = diffDate / (1000 * 3600 * 24);
-  //   return diffDate;
-  // };
-
-  const toDateFormatting = (year, month, date) => {
-    return `${year}-${String(month).padStart(2, 0)}-${String(date).padStart(
-      2,
-      0
-    )}`;
-  };
-
-  // calendar 를 넘길 때 데이터 fetch
-  const fetchSchedule = async (valDay) => {
-    const prevVal = {
-      year: valDay.year,
-      month: valDay.month - 1,
-      date: 1,
-    };
-    const nextVal = {
-      year: valDay.year,
-      month: valDay.month + 1,
-      date: 31,
-    };
-    if (valDay.month === 12) {
-      nextVal.year = valDay.year + 1;
-      nextVal.month = 1;
-    }
-    if (valDay.month === 1) {
-      prevVal.year = valDay.year - 1;
-      prevVal.month = 12;
-    }
-    const prev = toDateFormatting(prevVal.year, prevVal.month, prevVal.date);
-    const next = toDateFormatting(nextVal.year, nextVal.month, nextVal.date);
-
-    fetchOption = {
-      method: "POST",
-      body: JSON.stringify({ prev, next }),
-      headers: { "Content-Type": "application/json" },
-    };
-    const result = await fetch("/main/schedule", fetchOption).then((res) =>
-      res.json()
-    );
-    return result;
-  };
-
-  const showSchedule = (conData) => {
+  const setSchedule = (conData) => {
     let schedule;
     let dates = Array.from(document.querySelectorAll(".date"));
     for (let date of dates) {
-      let classArr = Array.from(date.classList);
-      classArr = String(classArr.filter((ele) => ele.includes("-")));
-      if (classArr === "") {
+      let classArr = !!String(
+        Array.from(date.classList).filter((ele) => ele.includes("-"))
+      );
+      if (!classArr) {
         continue;
       }
       let lastconCode;
@@ -124,101 +112,105 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  // !! showDate 함수를 기능별로 분할해야 함 !!
-  const showDate = async (valDay) => {
-    const todayVal = toDateFormatting(today.year, today.month, today.date);
+  class CalendarDate {
+    constructor() {
+      const currMonth = new Date(selectedDate.year, selectedDate.month, 0);
+      const prevMonth = new Date(selectedDate.year, selectedDate.month - 1, 0);
+      this.currMonthLastDate = currMonth.getDate();
+      this.prevMonthLastDate = prevMonth.getDate();
+      this.prevMonthLastDay = prevMonth.getDay();
+      this.index = {
+        prev: this.prevMonthLastDate - this.prevMonthLastDay,
+        curr: 1,
+        next: 1,
+      };
+    }
 
-    // lastDate: 이번 달 마지막 날짜 = 이번 달 날짜의 총 개수
-    const lastDate = new Date(valDay.year, valDay.month, 0).getDate();
-    // prevLastDate: 저번 달 마지막 날짜
-    const prevLastDate = new Date(valDay.year, valDay.month - 1, 0).getDate();
-    // prevMonthDays: 저번 달 마지막 날짜 요일 index + 1. 0(일요일)부터 시작하므로 요일 개수를 구하기 위해 + 1
-    // 이번 달 첫 날짜 요일이 언제인지를 구하기 위함
-    const prevMonthDays =
-      new Date(valDay.year, valDay.month - 1, 0).getDay() + 1;
-    /**
-     * dIndex.current : 1 ~ lastDate 까지 증가
-     * dIndex.prev : 이번 달 첫 주에서 표시할 저번 달의 시작 날짜(prevLastDate - 이전 요일 Index)
-     *               prevMonthDays에서 + 1 했으므로 - 1
-     */
+    increasePrev() {
+      this.index.prev++;
+    }
+    increaseCurr() {
+      this.index.curr++;
+    }
+    increaseNext() {
+      this.index.next++;
+    }
 
-    const dIndex = {
-      current: 1,
-      prev: prevLastDate - (prevMonthDays - 1),
-      next: 1,
-    };
+    get prevMonthDateClass() {
+      const isFirstMonth = selectedDate.month === 1;
+      return getFormattedDate(
+        isFirstMonth ? selectedDate.year - 1 : selectedDate.year,
+        isFirstMonth ? 12 : selectedDate.month - 1,
+        this.index.prev
+      );
+    }
+    get currMonthDateClass() {
+      return getFormattedDate(
+        selectedDate.year,
+        selectedDate.month,
+        this.index.curr
+      );
+    }
+    get nextMonthDateClass() {
+      const isLastMonth = selectedDate.month === 12;
+      return getFormattedDate(
+        isLastMonth ? selectedDate.year + 1 : selectedDate.year,
+        isLastMonth ? 1 : selectedDate.month + 1,
+        this.index.next
+      );
+    }
+    get todayMonthDateClass() {
+      return getFormattedDate(today.year, today.month, today.date);
+    }
+  }
 
-    tbody.textContent = "";
+  const setDateClassName = () => {
+    const calDate = new CalendarDate();
+    const isPrevMonth = (row, col) =>
+      row === 0 &&
+      calDate.prevMonthLastDay != 6 &&
+      col <= calDate.prevMonthLastDay;
+    const isCurrMonth = () => calDate.index.curr <= calDate.currMonthLastDate;
 
-    for (let j = 0; j < 6; j++) {
-      let tr = document.createElement("div");
-      tr.className = "week";
-
-      for (let k = 0; k < 7; k++) {
-        let td = document.createElement("div");
-        td.className = "date";
-        let dateTxt = document.createElement("div");
-        dateTxt.classList.add("date_txt");
-        td.appendChild(dateTxt);
-
-        // 저번 달 날짜 표시
-        // prevMonthDays != 7: 저번 달 마지막 요일이 토요일(6 + 1 = 7)이면 이번 달은 일요일이므로 첫 주 공백이 없음
-        if (j === 0 && prevMonthDays != 7 && k < prevMonthDays) {
-          dateTxt.textContent = dIndex.prev;
-          let dd = String(dIndex.prev).padStart(2, 0);
-          if (valDay.month === 1) {
-            td.classList.add(`${valDay.year - 1}-12-${dd}`);
-          } else {
-            let mm = String(valDay.month - 1).padStart(2, 0);
-            td.classList.add(`${valDay.year}-${mm}-${dd}`);
+    Array.from(tbody.children).forEach((week, row) => {
+      Array.from(week.children).forEach((date, col) => {
+        const dateTxt = date.querySelector(".date_txt");
+        if (isPrevMonth(row, col)) {
+          dateTxt.textContent = calDate.index.prev;
+          const className = calDate.prevMonthDateClass;
+          date.classList.add(className, "prevMonth");
+          calDate.increasePrev();
+        } else if (isCurrMonth()) {
+          dateTxt.textContent = calDate.index.curr;
+          const className = calDate.currMonthDateClass;
+          const todayClassName = calDate.todayMonthDateClass;
+          date.classList.add(className, "currMonth");
+          if (className === todayClassName) {
+            date.classList.add("today");
           }
-          td.classList.add("prevMonth");
-          dIndex.prev++;
-
-          // 이번 달 날짜 표시
-        } else if (dIndex.current <= lastDate) {
-          dateTxt.textContent = dIndex.current;
-          let mm = String(valDay.month).padStart(2, 0);
-          let dd = String(dIndex.current).padStart(2, 0);
-          td.classList.add(`${valDay.year}-${mm}-${dd}`);
-          const tdClassArr = Array.from(td.classList);
-          if (matchDay(tdClassArr, todayVal)) {
-            td.classList.add("today");
-          }
-          dIndex.current++;
-
-          // 다음 달 날짜 표시
+          calDate.increaseCurr();
         } else {
-          dateTxt.textContent = dIndex.next;
-          let dd = String(dIndex.next).padStart(2, 0);
-          if (valDay.month === 12) {
-            td.classList.add(`${valDay.year + 1}-01-${dd}`);
-          } else {
-            let mm = String(valDay.month + 1).padStart(2, 0);
-            td.classList.add(`${valDay.year}-${mm}-${dd}`);
-          }
-          td.classList.add("nextMonth");
-          dIndex.next++;
+          dateTxt.textContent = calDate.index.next;
+          const className = calDate.nextMonthDateClass;
+          date.classList.add(className, "nextMonth");
+          calDate.increaseNext();
         }
-        tr.appendChild(td);
-      } // 1주 for문 종료
-      tbody.appendChild(tr);
-    } // 1달 for문 종료
+      });
+    });
+  };
 
-    showNum();
-
-    // 공휴일 데이터 표시
+  const setDateHoliday = () => {
     const tds = Array.from(document.querySelectorAll(".date"));
     for (let td of tds) {
-      let tdClassArr = Array.from(td.classList);
-      tdClassArr = tdClassArr.map((tdClass) => {
+      const tdClassArr = Array.from(td.classList).map((tdClass) => {
         if (tdClass.includes("-")) {
           tdClass = tdClass.replaceAll("-", "");
         }
         return tdClass;
       });
-      let dateTxt = td.querySelector(".date_txt");
-      let holiTxt = document.createElement("div");
+
+      const dateTxt = td.querySelector(".date_txt");
+      const holiTxt = document.createElement("div");
       holiTxt.classList.add("holi_txt");
       td.appendChild(holiTxt);
       for (let i of holiData) {
@@ -231,34 +223,62 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       }
     }
-    const result = await fetchSchedule(valDay);
-    showSchedule(result);
   };
 
-  showDate(valDay);
+  const setCalendar = () => {
+    tbody.textContent = "";
+
+    const rows = Array.from({ length: 6 }).map(() => {
+      const tr = document.createElement("div");
+      tr.className = "week";
+      const cols = Array.from({ length: 7 }).map(() => {
+        const td = document.createElement("div");
+        td.className = "date";
+        const dateTxt = document.createElement("div");
+        dateTxt.classList.add("date_txt");
+        td.appendChild(dateTxt);
+        return td;
+      });
+      tr.append(...cols);
+      return tr;
+    });
+    tbody.append(...rows);
+
+    setDateClassName();
+    setDateHoliday();
+  };
+
+  const showCalendar = async (selectedDate) => {
+    const result = await fetchSchedule(selectedDate);
+    setTodayText();
+    setCalendar();
+    setSchedule(result);
+  };
+
+  showCalendar(selectedDate);
 
   btnPrev?.addEventListener("click", () => {
-    valDay.month--;
-    if (valDay.month === 0) {
-      valDay.month = 12;
-      valDay.year--;
+    selectedDate.month--;
+    if (selectedDate.month === 0) {
+      selectedDate.month = 12;
+      selectedDate.year--;
     }
-    showNum();
-    showDate(valDay);
+    showCalendar(selectedDate);
   });
+
   btnNext?.addEventListener("click", () => {
-    valDay.month++;
-    if (valDay.month === 13) {
-      valDay.month = 1;
-      valDay.year++;
+    selectedDate.month++;
+    if (selectedDate.month === 13) {
+      selectedDate.month = 1;
+      selectedDate.year++;
     }
-    showNum();
-    showDate(valDay);
+    showCalendar(selectedDate);
   });
+
   btnToday?.addEventListener("click", () => {
-    valDay.year = today.year;
-    valDay.month = today.month;
-    valDay.date = today.date;
-    showDate();
+    selectedDate.year = today.year;
+    selectedDate.month = today.month;
+    selectedDate.date = today.date;
+    showCalendar(selectedDate);
   });
 });
